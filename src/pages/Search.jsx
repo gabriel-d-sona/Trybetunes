392ac5658aab306f84075af1d3d0e52a1be0fe64
gabriel-d-sona <gdsona@hotmail.com>
2023-04-17 17:05:30 -0300

    requisito 5 feito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,44 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    name: '',
+    btnDisable: true,
+  };
+
+  onInputChange = (event) => {
+    const { target } = event;
+    const { value, name } = target;
+
+    this.setState({
+      [name]: value,
+      btnDisable: value.length <= 1,
+    });
+  };
+
   render() {
+    const { name, btnDisable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <div>
+          <input
+            id="name-input"
+            data-testid="search-artist-input"
+            type="text"
+            name="name"
+            value={ name }
+            onChange={ this.onInputChange }
+          />
+          <button
+            id="btn"
+            data-testid="search-artist-button"
+            disabled={ btnDisable }
+            onClick={ this.btn }
+          >
+            Entrar
+          </button>
+        </div>
       </div>
     );
   }
